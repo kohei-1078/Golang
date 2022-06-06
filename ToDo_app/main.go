c7380e778081f47dee97b30a217c1952592b238c
kohei-1078 <k.toyama1997@gmail.com>
2022-06-06 09:25:00 +0900

    start making ToDoApp, \nFisrt, config and logging settings

---
+++ ToDo_app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"ToDo_app/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+
+	log.Println("test")
+
+}

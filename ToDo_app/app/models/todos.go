afb9e066f049634a6d33e298192ed5444b204a8e
kohei-1078 <k.toyama1997@gmail.com>
2022-06-06 22:56:45 +0900

    database/*/

---
+++ ToDo_app/app/models/todos.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"log"
+	"time"
+)
+
+type Todo struct {
+	ID int
+	Content string
+	UserID int
+	CreatedAt time.Time
+}
+
+func (u *User) CreateTodo(content string) (err error) {
+	cmd := `insert into todos (
+		content,
+		user_id,
+		create_at) values (?, ?, ?)`
+	
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetTodo(id int) (todo Todo, err error) {
+	cmd := `select id, content, user_id, create_at from todos where id = ?`
+	todo = Todo{}
+	
+	err = Db.QueryRow(cmd, id).Scan(
+		&todo.ID, 
+		&todo.Content, 
+		&todo.UserID, 
+		&todo.CreatedAt)
+
+	return todo, err
+}
--- ToDo_app/main.go
+++ ToDo_app/main.go
@@ -2,16 +2,50 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"ToDo_app/config"
+	// "log"
+	// "ToDo_app/config"
+	"ToDo_app/app/models"
 )
 
 func main() {
+	/*
 	fmt.Println(config.Config.Port)
 	fmt.Println(config.Config.SQLDriver)
 	fmt.Println(config.Config.DbName)
 	fmt.Println(config.Config.LogFile)
 
 	log.Println("test")
+	*/
 
+	fmt.Println(models.Db)
+
+	/*
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	u, _ := models.GetUser(1)
+
+	fmt.Println(u)
+
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
+	*/
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }

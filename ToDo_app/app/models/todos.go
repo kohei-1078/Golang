cd6dd237b6549e3b09392a549a49f08d65dddc4c
kohei-1078 <k.toyama1997@gmail.com>
2022-06-08 23:57:48 +0900

    add todos function in todos.go

--- ToDo_app/app/models/todos.go
+++ ToDo_app/app/models/todos.go
@@ -57,3 +57,26 @@ func GetTodos() (todos []Todo, err error) {
 	}
 	return todos, err
 }
+
+func (u *User) GetTodosByUser() (todos []Todo, err error) {
+	cmd := `select id, content, user_id, create_at from todos
+	where user_id = ?`
+
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, 
+			&todo.Content, 
+			&todo.UserID, 
+			&todo.CreatedAt)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todos = append(todos, todo)
+	}
+	return todos, err
+}
--- ToDo_app/main.go
+++ ToDo_app/main.go
@@ -16,14 +16,14 @@ func main() {
 
 	log.Println("test")
 	*/
+	/*
 
 	fmt.Println(models.Db)
 
-	/*
 
 	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
+	u.Name = "test2"
+	u.Email = "test2@example.com"
 	u.PassWord = "testtest"
 	fmt.Println(u)
 
@@ -48,12 +48,18 @@ func main() {
 
 	t, _ := models.GetTodo(1)
 	fmt.Println(t)
+
+	user, _ := models.GetUser(3)
+	user.CreateTodo("Third Todo")
 	*/
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
 
-	todos, _ := models.GetTodos()
+	user2, _ := models.GetUser(2)
+	todos, _ := user2.GetTodosByUser()
 	for _, v := range todos {
 		fmt.Println(v)
 	}

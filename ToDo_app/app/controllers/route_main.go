1deb7f1e21b7f2631d8ed787743c5d53f7e8d3ae
kohei-1078 <k.toyama1997@gmail.com>
2022-06-10 18:51:25 +0900

    ユーザー登録画面作成

--- ToDo_app/app/controllers/route_main.go
+++ ToDo_app/app/controllers/route_main.go
@@ -1,14 +1,11 @@
 package controllers
 
 import (
-	"html/template"
+	// "log"
+	// "html/template"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, "Hello")
+	generateHTML(w, "Hello", "layout", "public_navbar", "top")
 }

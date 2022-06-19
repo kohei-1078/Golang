f97d9a51a6d3073d71dd116ad2da20f342350fda
kohei-1078 <k.toyama1997@gmail.com>
2022-06-20 00:00:55 +0900

    section22

--- Golang_udemy/section22/main.go
+++ Golang_udemy/section22/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 	// "time"
+	// "math/rand"
 	"runtime"
 )
 
@@ -50,7 +51,58 @@ type Person struct {
 }
 
 func main() {
+	smap := &sync.Map{}
+
+	smap.Store("Hello", "World")
+	smap.Store(1, 2)
+
+	smap.Range(func(key, value interface{}) bool{
+		fmt.Println(key, value)
+		return true
+	})
+
+	smap.Delete(1)
+
+	smap.Range(func(key, value interface{}) bool{
+		fmt.Println(key, value)
+		return true
+	})
 	
+	v, ok := smap.Load("Hello")
+	if ok {
+		fmt.Println(v)
+	}
+
+	smap.LoadOrStore("Hello", "Woooooorld")
+	smap.LoadOrStore(2, 3)
+
+	smap.Range(func(key, value interface{}) bool{
+		fmt.Println(key, value)
+		return true
+	})
+
+	// var wg sync.WaitGroup
+	// m := map[string]int{"A": 0, "B": 1}
+
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(2)
+
+	// 	go func() {
+	// 		defer wg.Done()
+
+	// 		m["A"] = rand.Intn(100)
+	// 		m["B"] = rand.Intn(100)
+	// 	}()
+
+	// 	go func() {
+	// 		defer wg.Done()
+
+	// 		m["A"] = rand.Intn(100)
+	// 		m["B"] = rand.Intn(100)
+	// 	}()
+	// }
+
+	// wg.Wait()
 
 	// count := 0
 	// mypool := & sync.Pool{

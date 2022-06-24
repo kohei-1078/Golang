3157c8cbeb6368819cbc6232fb8cbb7c65c954e3
kohei-1078 <k.toyama1997@gmail.com>
2022-06-25 00:27:22 +0900

    mypractice

--- Golang_udemy/myworkspace/main.go
+++ Golang_udemy/myworkspace/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"Golang_udemy/myworkspace/person"
+	// "sync"
+	// "Golang_udemy/myworkspace/person"
 	"time"
+	"os"
+	"bufio"
+	"strconv"
+	"log"
 )
 
 func add(x, y int) int {
@@ -15,80 +19,81 @@ func sub(x, y int) int {
 	return x - y
 }
 
+func printGoodMorning() {
+	fmt.Println("Good Morning")
+}
+
+func printHello() {
+	fmt.Println("Hello")
+}
+
+func printGoodEvening() {
+	fmt.Println("Good Evening")
+}
+
 func main() {
-	var wg sync.WaitGroup
-
+	// var wg sync.WaitGroup
+	// message := 0
 	ch1 := make(chan int)
-	ch2 := make(chan int)
 
-	done := make(chan interface{})
-
-	go func() {
-		time.Sleep(2 * time.Second)
-		close(done)
-	}()
-
-	go func() {
-		defer close(ch1)
-		for i := 0; i < 10; i++ {
-			time.Sleep(time.Second)
-			ch1 <- i
-		}
-	}()
-
-	go func() {
-		defer close(ch2)
-		for i := 0; i < 10; i++ {
-			time.Sleep(time.Second)
-			ch2 <- i
-		}
-	}()
-
-	loop:
 	for {
-		select{
-		case <- done:
-			break loop
-		case v, ok := <-ch1:
-			if !ok {
-				continue loop
-			// 	break loop
+		time.Sleep(2 * time.Second)
+		go func() {
+			fmt.Print("input number(1~4)?\n")
+			// Scannerを使って一行読み
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			mem := scanner.Text()
+			message, err := strconv.Atoi(mem)
+			ch1 <- message
+			if err != nil {
+				log.Println(err)
+			}// else {
+			// 	fmt.Printf("%v\n%T\n", message, message)
+			// }
+		}()
+
+		go func() {
+			time.Sleep(10 * time.Second)
+			x := <-ch1
+			// fmt.Println(x)
+			switch {
+			case x == 1:
+				printGoodMorning()
+			case x == 2:
+				printHello()
+			case x == 3:
+				printGoodEvening()
+			default:
+				fmt.Println("No input")
 			}
-			fmt.Printf("ch1: %v\n", v)
-		case v, ok := <-ch2:
-			if !ok {
-				continue loop
-				// break loop
-			}
-			fmt.Printf("ch2: %v\n", v)
-		}
+		}()
+
 	}
-
-
 	// fmt.Println("Hello World!")
 	// fmt.Println(add(1,2))
 	// fmt.Println(sub(1,2))
 
 
-	var personList person.PersonList
-	p1 := person.Person{"Yamada Taro", 24}
-	p2 := person.Person{"Bond", 55}
-	p3 := person.Person{"White", 72}
-	personList = append(personList, &p1, &p2, &p3)
+	// var personList person.PersonList
+	// p1 := person.Person{"Yamada Taro", 24}
+	// p2 := person.Person{"Bond", 55}
+	// p3 := person.Person{"White", 72}
+	// personList = append(personList, &p1, &p2, &p3)
 
 	// fmt.Printf("Name: %v\nAge: %v\n", p1.Name, p1.Age)
 	
-	wg.Add(2)
-	go person.UpdatePersonName(&wg, &p1, "Yamada Hanako")
-	go person.UpdatePersonAge(&wg, &p1, 22)
-	wg.Wait()
+	// wg.Add(2)
+	// go person.UpdatePersonName(&wg, &p1, "Yamada Hanako")
+	// go person.UpdatePersonAge(&wg, &p1, 22)
+	// wg.Wait()
 
-	personList = person.RemovePerson(personList, 0)
+	// personList = person.RemovePerson(personList, 0)
 	
 	// fmt.Printf("Name: %v\nAge: %v\n", p1.Name, p1.Age)
 	// fmt.Println(personList)
-	for _, v := range personList {
-		fmt.Println(*v)
-	}
+	// for _, v := range personList {
+	// 	fmt.Println(*v)
+	// }
 
 }
